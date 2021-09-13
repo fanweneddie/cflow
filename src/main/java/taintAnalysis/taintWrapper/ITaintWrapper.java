f1cd32361b9d4f6ac2610a945fcf0b7977f36599
fanweneddie <1164087609@qq.com>
2021-09-14 00:18:41 +0800

    add UniqueStmt

--- src/main/java/taintAnalysis/taintWrapper/ITaintWrapper.java
+++ src/main/java/taintAnalysis/taintWrapper/ITaintWrapper.java
@@ -3,6 +3,7 @@ package taintAnalysis.taintWrapper;
 import soot.SootMethod;
 import soot.jimple.Stmt;
 import taintAnalysis.Taint;
+import taintAnalysis.UniqueStmt;
 
 import java.util.Map;
 import java.util.Set;
@@ -15,14 +16,14 @@ public interface ITaintWrapper {
      * the wrapper to artificially propagate taints over method invocations without
      * requiring the analysis to look inside the method.
      * @param in            The in-set of taints before the invocation statement
-     * @param stmt          The invocation statement which to check for black-box taint propagation
+     * @param uniqueStmt    The invocation statement which to check for black-box taint propagation
      * @param caller        The caller method of the invocation statement
      * @param killSet       The kill sets of the invocation statement computed with the provided in-set
      * @param genSet        The gen sets of the invocation statement computed with the provided in-set
      * @param taintCache    The taint cache of the caller method,
      *                       used to ensure global uniqueness of generated taint objects
      */
-    void genTaintsForMethodInternal(Set<Taint> in, Stmt stmt, SootMethod caller,
+    void genTaintsForMethodInternal(Set<Taint> in, UniqueStmt uniqueStmt, SootMethod caller,
                                     Set<Taint> killSet, Set<Taint> genSet,
                                     Map<Taint, Taint> taintCache);
 
