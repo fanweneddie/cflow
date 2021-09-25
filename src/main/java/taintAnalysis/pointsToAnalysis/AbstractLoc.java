7a8c14450c941cb057b1494713cad840fc172605
fanweneddie <1164087609@qq.com>
2021-09-25 11:17:45 +0800

    add constant pool type

--- src/main/java/taintAnalysis/pointsToAnalysis/AbstractLoc.java
+++ src/main/java/taintAnalysis/pointsToAnalysis/AbstractLoc.java
@@ -2,10 +2,10 @@ package taintAnalysis.pointsToAnalysis;
 
 import soot.Type;
 import soot.SootMethod;
+import soot.Value;
 
 import taintAnalysis.UniqueStmt;
 
-import java.util.Queue;
 
 /**
  * The abstract location on heap,
@@ -14,35 +14,53 @@ import java.util.Queue;
 public class AbstractLoc {
     // the method that the allocation site is in
     private final SootMethod method;
-    // the maximal number of nearest call statements
-    // or that is k in k-limiting callString
-    private final int callStringLen;
-    // the string of call statements as a context
-    private final Queue<UniqueStmt> callString;
+    // the context of method
+    private final Context context;
     // the invoke statement of allocation.
     private final UniqueStmt allocStmt;
     // the type of object
     private final Type type;
+    // whether the AbstractLoc is in constant pool
+    private final boolean isConst;
+    // if the AbstractLoc is in constant pool, constValue is its value
+    private final Value constValue;
+    // an unknown abstract location for initialization
+    private static final AbstractLoc unknown = new AbstractLoc(null, null, null, null);
 
     /**
-     * Constructor: it is assumed that allocStmt is an init statement
+     * Constructor: construct an abstract location that is not in constant pool
+     * it is assumed that allocStmt is an init statement
      */
-    public AbstractLoc(SootMethod method, int callStringLen, Queue<UniqueStmt> callString, UniqueStmt allocStmt, Type type) {
+    public AbstractLoc(SootMethod method, Context context, UniqueStmt allocStmt, Type type) {
         this.method = method;
-        this.callStringLen = callStringLen;
-        this.callString = callString;
+        this.context = context;
         this.allocStmt = allocStmt;
         this.type = type;
+        this.isConst = false;
+        this.constValue = null;
+    }
+
+    public AbstractLoc(Type type, Value constValue) {
+        this.method = null;
+        this.context = null;
+        this.allocStmt = null;
+        this.type = type;
+        this.isConst = false;
+        this.constValue = constValue;
     }
 
     public SootMethod getMethod() { return method; }
 
-    public int getCallStringLen() { return callStringLen; }
-
-    public Queue<UniqueStmt> getCallString() { return callString; }
+    public Context getContext() { return context; }
 
     public UniqueStmt getAllocStmt() { return allocStmt; }
 
     public Type getType() { return type; }
+
+    public boolean getIsConst() { return isConst; }
+
+    public Value getConstValue() { return constValue; }
+
+    public static AbstractLoc getKnown() { return unknown; }
 
 }
