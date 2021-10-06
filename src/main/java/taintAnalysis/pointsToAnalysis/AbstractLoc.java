cbf6028bc498b120551f5e8493a59e6fed375ff4
fanweneddie <1164087609@qq.com>
2021-10-06 21:16:43 +0800

    delete the field for const

--- src/main/java/taintAnalysis/pointsToAnalysis/AbstractLoc.java
+++ src/main/java/taintAnalysis/pointsToAnalysis/AbstractLoc.java
@@ -20,33 +20,18 @@ public class AbstractLoc {
     private final UniqueStmt allocStmt;
     // the type of object
     private final Type type;
-    // whether the AbstractLoc is in constant pool
-    private final boolean isConst;
-    // if the AbstractLoc is in constant pool, constValue is its value
-    private final Value constValue;
     // an unknown abstract location for initialization
     private static final AbstractLoc unknown = new AbstractLoc(null, null, null, null);
 
     /**
      * Constructor: construct an abstract location that is not in constant pool
-     * it is assumed that allocStmt is an init statement
+     * it is assumed that allocStmt is a call statement to init()
      */
     public AbstractLoc(SootMethod method, Context context, UniqueStmt allocStmt, Type type) {
         this.method = method;
         this.context = context;
         this.allocStmt = allocStmt;
         this.type = type;
-        this.isConst = false;
-        this.constValue = null;
-    }
-
-    public AbstractLoc(Value constValue) {
-        this.method = null;
-        this.context = null;
-        this.allocStmt = null;
-        this.type = constValue.getType();
-        this.isConst = true;
-        this.constValue = constValue;
     }
 
     public SootMethod getMethod() { return method; }
@@ -56,10 +41,6 @@ public class AbstractLoc {
     public UniqueStmt getAllocStmt() { return allocStmt; }
 
     public Type getType() { return type; }
-
-    public boolean getIsConst() { return isConst; }
-
-    public Value getConstValue() { return constValue; }
 
     public static AbstractLoc getKnown() { return unknown; }
 
