bebbad78bf433eaa5a4d92587f162d8108ac77f5
fanweneddie <1164087609@qq.com>
2021-10-15 18:14:27 +0800

    add comments for Context

--- src/main/java/taintAnalysis/pointsToAnalysis/Context.java
+++ src/main/java/taintAnalysis/pointsToAnalysis/Context.java
@@ -1,81 +1,82 @@
 package taintAnalysis.pointsToAnalysis;
 
 import taintAnalysis.UniqueStmt;
-import taintAnalysis.pointsToAnalysis.pointsToSet.PointsToSet;
 
 import java.util.*;
 
 /**
- * The context of a method,
- * which contains callString and The points-to set of this object, return value and arguments
+ * The context of a statement,
+ * which contains a k-limiting call string
  */
 public class Context {
-    // the maximal number of the nearest call statements
+    // The maximal number of the nearest call statements
     // or that is k in k-limiting callString
-    private final int callStringLen;
-    // the string of call statements as a context
-    private final Queue<UniqueStmt> callString;
-    // the list of points-to map of this object and arguments
-    // its size = number of arguments + 2
-    private final List<PointsToSet> summary;
+    private final int maxCallStringLen;
+    // The string of call statements,
+    // where the first statement is the nearest statement,
+    // and the later statements are reversed call statements.
+    // e.g. for object o of program below:
+    // -----------------------------
+    // int main() { s1: foo(); }
+    // void foo() { s2: bar(); }
+    // void bar() { s3: T o = new T(); }
+    // ------------------------------
+    // In method bar(), o's context is [s3],
+    // In method foo(), o's context is [s3,s2],
+    // In method main(), o's context is [s3,s2,s1]
+    private final List<UniqueStmt> callString;
+
+    /** Naive constructor */
+    public Context(int maxCallStringLen, UniqueStmt allocCallStmt) {
+        this.maxCallStringLen = maxCallStringLen;
+        this.callString = new LinkedList<>();
+        this.callString.add(allocCallStmt);
+    }
+
+    /** Copy constructor */
+    public Context(Context context) {
+        this.maxCallStringLen = context.getMaxCallStringLen();
+        this.callString = new LinkedList<>();
+        this.callString.addAll(context.getCallString());
+    }
 
     /**
-     * Construct the context by passing callString and its max length
-     * We need to check whether the given callString is within the max length,
-     * If not, we should use k-limiting way to approximate callString
-     * @param callStringLen
-     * @param callString
-     * @param argNum        number of arguments in the method that this context is in.
+     * Add an allocation call statement into callString.
+     * Note that we should do an approximation of maxCallStringLen.
+     * @param allocCallStmt     the new allocation call statement
+     * @return                  true iff allocCallStmt is successfully added
      */
-    public Context(int callStringLen, Queue<UniqueStmt> callString, int argNum) {
-        this.callStringLen = callStringLen;
-        // approximate callString with given callStringLen
-        this.callString = approximateCallString(callString);
-        // initialize summary with null
-        int summarySize = argNum + 2;
-        this.summary = new ArrayList<>(summarySize);
-        for( int i = 0; i < summarySize; ++i) {
-            summary.add(null);
+    public boolean addAllocCallStmt(UniqueStmt allocCallStmt) {
+        if (callString.size() < maxCallStringLen) {
+            return callString.add(allocCallStmt);
+        } else {
+            return false;
         }
     }
 
-    /**
-     * Make the k-limiting of the callString as a context
-     * if the length of callString is greater than callStringLen,
-     * we only keep the callStringLen nearest call sites
-     * @param callString        the input queue of call sites
-     * @return                  the callStringLen-approximate call sites
-     */
-    private Queue<UniqueStmt> approximateCallString(Queue<UniqueStmt> callString) {
-        // get a clone of callString
-        Queue<UniqueStmt> newCallString = new LinkedList<>();
-        newCallString.addAll(callString);
+    public int getMaxCallStringLen() { return maxCallStringLen; }
 
-        // remove call site at the tail of the queue
-        // until there are only callStringLen call sites
-        while(newCallString.size() > callStringLen) {
-            newCallString.remove();
+    public List<UniqueStmt> getCallString() { return callString; }
+
+    @Override
+    public String toString() {
+        String str = "callString:\n";
+        for (UniqueStmt allocStmt : callString) {
+            str += allocStmt.toString();
+            str += "\n";
         }
-        return newCallString;
+        return str;
     }
 
-    /**
-     * Set the points-to map of an object(this/return value/argument)
-     * We use strong update to replace.
-     * @param index             the index of the object in summary
-     *                          by default, summary[0] is for this object,
-     *                          summary[1] is for return value
-     *                          summary[i+2] is for the ith argument
-     * @param pointsToSet      the points-to set of an object
-     */
-    public void setSummary(int index, PointsToSet pointsToSet) {
-        summary.set(index, pointsToSet);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Context context = (Context) o;
+        return maxCallStringLen == context.maxCallStringLen &&
+                Objects.equals(callString, context.callString);
     }
-
-    public int getCallStringLen() { return callStringLen; }
-
-    public Queue<UniqueStmt> getCallString() { return callString; }
-
-    public List<PointsToSet> getSummary() { return summary; }
 
 }
