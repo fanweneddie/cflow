e553f246a6ee308a1ed096786d70ffd5313c04b5
fanweneddie <1164087609@qq.com>
2021-10-15 18:17:31 +0800

    add comments for ObjRefPointsToSet

--- src/main/java/taintAnalysis/pointsToAnalysis/pointsToSet/ObjRefPointsToSet.java
+++ src/main/java/taintAnalysis/pointsToAnalysis/pointsToSet/ObjRefPointsToSet.java
@@ -11,38 +11,33 @@ import java.util.*;
 
 import static assertion.Assert.assertNotNull;
 
-/*
+/**
 * This class describes a points-to set of a reference to an object.
 * Beside the location set of this reference,
 * it also contains the points-to set of each reference field.
 */
 public class ObjRefPointsToSet extends PointsToSet {
 
-    // the map from variables to points-to set of the field object
+    // The map from variables to points-to set of the field object
     private final Map<SootField, PointsToSet> fieldPtm;
 
-    /* Several naive constructors for ObjRefPointsToSet */
+    /** Several naive constructors for ObjRefPointsToSet */
     public ObjRefPointsToSet() {
         super();
         this.fieldPtm = new HashMap<>();
     }
 
-    public ObjRefPointsToSet(Set<AbstractLoc> locSet) {
-        super(locSet);
+    public ObjRefPointsToSet(AbstractLoc location) {
+        super(location);
         this.fieldPtm = new HashMap<>();
     }
 
-    public ObjRefPointsToSet(AbstractLoc abstractLoc) {
-        super(abstractLoc);
-        this.fieldPtm = new HashMap<>();
-    }
-
-    /* Copy constructor */
+    /** Copy constructor */
     public ObjRefPointsToSet(ObjRefPointsToSet objRefPts) {
         super();
         assertNotNull(objRefPts);
-        // copy locSet
-        locSet.addAll(objRefPts.getLocSet());
+        // copy location
+        location = new AbstractLoc(objRefPts.getLocation());
 
         // copy fieldPtm
         this.fieldPtm = new HashMap<>();
@@ -50,30 +45,27 @@ public class ObjRefPointsToSet extends PointsToSet {
     }
 
     /**
-     * Create a new points-to set for a variable at a program point
-     * @param method        the method that the program point is in
+     * Create a new points-to set for a variable at a program point.
      * @param context       the context of the method
-     * @param uniqueStmt    the statement of program point
      * @param objectType    the type of the object, it must be an instance of RefType
      */
-    public ObjRefPointsToSet(SootMethod method, Context context, UniqueStmt uniqueStmt, Type objectType) {
+    public ObjRefPointsToSet(Context context, Type objectType) {
         super();
-        // precondition check
+        // Precondition check
         assert(objectType instanceof RefType);
 
-        // initialize the locSet for the base object and points-to map for fields
-        AbstractLoc baseLoc = new AbstractLoc(method, context, uniqueStmt, objectType);
-        this.locSet.add(baseLoc);
+        // Initialize the location for the base object and points-to map for fields
+        this.location = new AbstractLoc(context, objectType);
         this.fieldPtm = new HashMap<>();
 
-        // for a field, if it is a reference type, we set its points-to set
-        // also, for fields of each field, we set their points-to set as nullObjRefPts as an approximation
+        // For each field, if it is a reference type, we set its points-to set.
+        // Also, for fields of each field, we set their points-to set as nullObjRefPts as an approximation
         RefType refType = (RefType) objectType;
         Chain<SootField> fields = refType.getSootClass().getFields();
         for (SootField field : fields) {
             if (field.getType() instanceof RefType || field.getType() instanceof ArrayType) {
-                // construct the points-to set of a field based on its type
-                AbstractLoc fieldLoc = new AbstractLoc(method, context, uniqueStmt, field.getType());
+                // Construct the points-to set of a field based on its type
+                AbstractLoc fieldLoc = new AbstractLoc(context, field.getType());
                 PointsToSet fieldPts;
                 if (field.getType() instanceof RefType) {
                     fieldPts = new ObjRefPointsToSet(fieldLoc);
@@ -85,41 +77,48 @@ public class ObjRefPointsToSet extends PointsToSet {
         }
     }
 
-    /* A complete constructor */
-    public ObjRefPointsToSet(Set<AbstractLoc> locSet, Map<SootField, PointsToSet> fieldPtm) {
-        super(locSet);
+    /** A complete constructor */
+    public ObjRefPointsToSet(AbstractLoc location, Map<SootField, PointsToSet> fieldPtm) {
+        super(location);
         this.fieldPtm = fieldPtm;
     }
 
     public Map<SootField, PointsToSet> getFieldPtm() { return fieldPtm; }
 
     /**
-     * Merge this ObjRefPointsToSet with a given ObjRefPointsToSet
-     * We first merge the location set
-     * And then we merge the PointsToSet of their field objects
-     * Finally, the result of merge is saved in this object
-     * @param pts           the given PointsToSet, must be an ObjRefPointsToSet
+     * Merge this ObjRefPointsToSet with a given ObjRefPointsToSet.
+     * We first merge the location set.
+     * Then we merge the points-to set of their field objects.
+     * Finally, the result of merge is saved in this object.
+     * @param pts           the given points-to set, must be an ObjRefPointsToSet
      */
     public void merge(PointsToSet pts) {
-        // we have nothing to merge if pts is a null
+        // We have nothing to merge if pts is a null
         if (pts == null) {
             return;
         }
-        // we only merge pts of the same type
+
+        // We only merge pts of the same type
         assert(pts instanceof ObjRefPointsToSet);
         ObjRefPointsToSet objRefPts = (ObjRefPointsToSet) pts;
-        // we don't need to waste time merging two same objects
+
+        // We don't need to waste time merging two same objects
         if (this == objRefPts) {
             return;
         }
-        // merge the location set
-        locSet.addAll(objRefPts.getLocSet());
 
-        // recursively merge PointsToSet of fields
+        // Merge the location by intersection
+        if (location != pts.getLocation()) {
+            location = null;
+        }
+
+        // Recursively merge PointsToSet of fields
         for (Map.Entry<SootField, PointsToSet> entry : objRefPts.getFieldPtm().entrySet()) {
+            // Get each field and its points-to set in objRefPts
             SootField field = entry.getKey();
             PointsToSet fieldPts = entry.getValue();
-            // we should assume that the class of two points-to set equals
+            // Check whether the field is recorded in this fieldPtm
+            // 1. Merge the field points-to set
             if (fieldPtm.containsKey(field)) {
                 assert (fieldPtm.get(field).getClass() == fieldPts.getClass());
                 if (fieldPtm.get(field) == null) {
@@ -127,7 +126,9 @@ public class ObjRefPointsToSet extends PointsToSet {
                 } else {
                     fieldPtm.get(field).merge(fieldPts);
                 }
-            } else {
+            }
+            // 2. Add the points-to set for the field
+            else {
                 PointsToSet newPts;
                 if (fieldPts instanceof ObjRefPointsToSet) {
                     newPts = new ObjRefPointsToSet((ObjRefPointsToSet)fieldPts);
@@ -137,6 +138,29 @@ public class ObjRefPointsToSet extends PointsToSet {
                     newPts = null;
                 }
                 fieldPtm.put(field, newPts);
+            }
+        }
+    }
+
+    /**
+     * Append an allocation statement into the context of the location
+     * of current points-to set and field points-to set
+     * @param allocStmt     the newly added allocation statement
+     */
+    public void addContext(UniqueStmt allocStmt) {
+        // add context for the location if it is not null
+        if (location != null) {
+            location.getContext().addAllocCallStmt(allocStmt);
+        }
+        // add context for the points-to set of each field
+        for (Map.Entry<SootField, PointsToSet> entry : fieldPtm.entrySet()) {
+            PointsToSet fieldPts = entry.getValue();
+            if (fieldPts != null) {
+                if(fieldPts instanceof ObjRefPointsToSet) {
+                    ((ObjRefPointsToSet) fieldPts).addContext(allocStmt);
+                } else {
+                    ((ArrRefPointsToSet) fieldPts).addContext(allocStmt);
+                }
             }
         }
     }
@@ -160,18 +184,29 @@ public class ObjRefPointsToSet extends PointsToSet {
         return fieldPtm.get(field);
     }
 
-    /**
-     * Override the equals method.
-     * Note that we only need to check whether locSet equals he
-     */
+    @Override
+    public String toString() {
+        String str = "location:\n";
+        str += location.toString();
+        str += "\n";
+        for (Map.Entry<SootField, PointsToSet> entry : fieldPtm.entrySet()) {
+            str += entry.getKey();
+            str += " ";
+            if (entry.getValue() != null)
+                str += entry.getValue().getLocation().toString();
+            str += "\n";
+        }
+        return str;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o)
             return true;
         if (o == null || getClass() != o.getClass())
             return false;
-        PointsToSet pointsToSet = (PointsToSet) o;
-        return Objects.equals(locSet, pointsToSet.locSet);
+        ObjRefPointsToSet objRefPointsToSet = (ObjRefPointsToSet) o;
+        return Objects.equals(location, objRefPointsToSet.location)
+                && Objects.equals(fieldPtm, objRefPointsToSet.fieldPtm);
     }
-
 }
